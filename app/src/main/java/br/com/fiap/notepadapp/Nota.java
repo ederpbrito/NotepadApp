a822905851439f094d648d1ff05141b1b0a4ffca
logonrm <logonrm@fiap.com.br>
2017-11-27 23:09:35 -0200

    Initial commit

---
+++ app/src/main/java/br/com/fiap/notepadapp/Nota.java
@@ -0,0 +1,35 @@
+package br.com.fiap.notepadapp;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Nota {
+
+    private String id;
+    private String titulo;
+    @SerializedName(value = "descricao")
+    private String texto;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
